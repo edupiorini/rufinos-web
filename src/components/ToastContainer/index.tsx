584a8b5dc113f96852b07b6d3988f115beae5299
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-01-19 07:56:43 -0300

    toast structure set

--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
 
-import { Container } from './styles';
+import { Container, Toast } from './styles';
 
 const ToastContainer: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Toast hasDescription>
+        <FiAlertCircle size={20} />
+        <div>
+          <strong>Aconteceu um erro</strong>
+          <p>Não foi possível fazer login na aplicação</p>
+        </div>
+
+        <button type="button">
+          <FiXCircle size={18} />
+        </button>
+      </Toast>
+
+      <Toast type="success" hasDescription={false}>
+        <FiAlertCircle size={20} />
+        <div>
+          <strong>Aconteceu um erro</strong>
+        </div>
+
+        <button type="button">
+          <FiXCircle size={18} />
+        </button>
+      </Toast>
+
+      <Toast type="error" hasDescription>
+        <FiAlertCircle size={20} />
+        <div>
+          <strong>Aconteceu um erro</strong>
+          <p>Não foi possível fazer login na aplicação</p>
+        </div>
+
+        <button type="button">
+          <FiXCircle size={18} />
+        </button>
+      </Toast>
+    </Container>
+  );
 };
 
 export default ToastContainer;
