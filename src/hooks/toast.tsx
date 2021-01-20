51a1ec428052e952f71fb9a5c1aa6035e646a513
Eduardo Piorini <eduardopiorini@hotmail.com>
2021-01-20 06:54:55 -0300

    removeToast  func on button

--- src/hooks/toast.tsx
+++ src/hooks/toast.tsx
@@ -1,12 +1,14 @@
 import React, { createContext, useContext, useCallback, useState } from 'react';
+import { uuid } from 'uuidv4';
+
 import ToastContainer from '../components/ToastContainer';
 
 interface ToastContextData {
-  addToast(): void;
-  removeToast(): void;
+  addToast(message: Omit<ToastMessage, 'id'>): void;
+  removeToast(id: string): void;
 }
 
-interface ToastMessage {
+export interface ToastMessage {
   id: string;
   type: 'error' | 'success' | 'info';
   description: string;
@@ -16,19 +18,32 @@ interface ToastMessage {
 const ToastContext = createContext<ToastContextData>({} as ToastContextData);
 
 const ToastProvider: React.FC = ({ children }) => {
-  const [message, setMessages] = useState<ToastMessage[]>([]);
-  const addToast = useCallback(() => {
-    console.log('addToast');
-  }, []);
+  const [messages, setMessages] = useState<ToastMessage[]>([]);
 
-  const removeToast = useCallback(() => {
-    console.log('removeToast');
+  const addToast = useCallback(
+    ({ type, description, title }: Omit<ToastMessage, 'id'>) => {
+      const id = uuid();
+
+      const toast = {
+        id,
+        type,
+        title,
+        description,
+      };
+
+      setMessages(oldMessages => [...oldMessages, toast]);
+    },
+    [],
+  );
+
+  const removeToast = useCallback((id: string) => {
+    setMessages(state => state.filter(message => message.id !== id));
   }, []);
 
   return (
     <ToastContext.Provider value={{ addToast, removeToast }}>
       {children}
-      <ToastContainer />
+      <ToastContainer messages={messages} />
     </ToastContext.Provider>
   );
 };
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -51,7 +51,11 @@ const SignIn: React.FC = () => {
 
           formRef.current?.setErrors(errors);
         }
-        addToast();
+        addToast({
+          type: 'error',
+          title: 'erro na autenticação',
+          description: 'erro de login, verifique as credenciais',
+        });
       }
     },
     [addToast, signIn],
